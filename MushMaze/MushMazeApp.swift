99110f0ac5da890889e007f658de2dbbd4fb09eb
Elin <elin.simonsson@outlook.com>
2023-01-24 11:14:16 +0100

    Initial Commit

---
+++ MushMaze/MushMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MushMazeApp.swift
+//  MushMaze
+//
+//  Created by Elin Simonsson on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct MushMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
